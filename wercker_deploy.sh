2389ce96f5dd66231957fe2ba31f7b28708165d4
Guilherme Nunes <guinunesilva@gmail.com>
2014-12-01 18:06:07 -0200

    changed wercker.yml

--- wercker_deploy.sh
+++ wercker_deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo $WERCKER_DEPLOYTARGET_NAME
+
 if [ "$WERCKER_DEPLOYTARGET_NAME" = "dev" ]; then
 	scp -i $PRIVATEKEY_PATH app.xml root@104.236.20.173:/var/www/dev.hangoutsplay.com/public_html
 fi
