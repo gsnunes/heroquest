f22587e180033efa3e4280a99acb03e26fcc110b
Guilherme Nunes <guinunesilva@gmail.com>
2014-12-02 10:40:01 -0200

    test deploy

--- wercker_deploy.sh
+++ wercker_deploy.sh
@@ -3,5 +3,6 @@
 echo $WERCKER_DEPLOYTARGET_NAME
 
 if [ "$WERCKER_DEPLOYTARGET_NAME" = "dev" ]; then
+	sed "s/192.168.56.101/test/g" app.xml
 	scp -i $PRIVATEKEY_PATH app.xml root@104.236.20.173:/var/www/dev.hangoutsplay.com/public_html
 fi
