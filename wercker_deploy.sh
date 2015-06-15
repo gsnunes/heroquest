2be4951dd399e36f231617bb7c4453ac6cd305b8
Guilherme Nunes <guinunesilva@gmail.com>
2015-06-15 16:37:15 -0300

    fixed board click events

--- wercker_deploy.sh
+++ wercker_deploy.sh
@@ -4,7 +4,7 @@ echo $WERCKER_DEPLOYTARGET_NAME
 
 if [ "$WERCKER_DEPLOYTARGET_NAME" = "dev" ]; then
 	#sed -i 's!192.168.56.101!test.hangoutsplay.com!g' app.xml
-	scp -i $PRIVATEKEY_PATH app.xml root@104.236.20.173:/var/www/dev.hangoutsplay.com/public_html
+	scp -i $PRIVATEKEY_PATH assets/app.xml root@104.236.20.173:/var/www/dev.hangoutsplay.com/public_html
 fi
 
 if [ "$WERCKER_DEPLOYTARGET_NAME" = "test" ]; then
