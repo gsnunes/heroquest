3fba9be77bb962a88de4e314fecb62f0b2cc4550
Guilherme Nunes <guinunesilva@gmail.com>
2015-09-14 11:36:43 -0300

    test deploy script

--- wercker_deploy.sh
+++ wercker_deploy.sh
@@ -5,6 +5,7 @@ echo $WERCKER_DEPLOYTARGET_NAME
 if [ "$WERCKER_DEPLOYTARGET_NAME" = "dev" ]; then
 	sed -i 's#<!-- main-script -->#<script data-main="//localhost:1337/js/src/config" src="//localhost:1337/bower_components/requirejs/require.js" id="main-script"></script>#g' assets/app.xml
 	scp -i $PRIVATEKEY_PATH assets/app.xml root@104.236.20.173:/var/www/dev.hangoutsplay.com/public_html
+	ssh -i $PRIVATEKEY_PATH 'ls -l; ps -aux; whoami'
 fi
 
 if [ "$WERCKER_DEPLOYTARGET_NAME" = "test" ]; then
