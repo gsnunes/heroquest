ab95025ef50b59ddcc80428bcefb2af7fb0e1b22
Guilherme Nunes <guinunesilva@gmail.com>
2015-09-16 11:37:31 -0300

    improve build

--- wercker_deploy.sh
+++ wercker_deploy.sh
@@ -5,11 +5,11 @@ echo $WERCKER_DEPLOYTARGET_NAME
 if [ "$WERCKER_DEPLOYTARGET_NAME" = "dev" ]; then
 	sed -i 's#<!-- main-script -->#<script data-main="//localhost:1337/js/src/config" src="//localhost:1337/bower_components/requirejs/require.js" id="main-script"></script>#g' assets/app.xml
 	scp -i $PRIVATEKEY_PATH assets/app.xml root@104.236.20.173:/var/www/dev.hangoutsplay.com/public_html
-	ssh -i $PRIVATEKEY_PATH root@104.236.20.173 'ls -l; ps -aux; whoami'
 fi
 
 if [ "$WERCKER_DEPLOYTARGET_NAME" = "test" ]; then
 	sed -i 's!localhost:1337!test.hangoutsplay.com!g' assets/app.xml
 	sed -i 's#<!-- main-script -->#<script src="//test.hangoutsplay.com/js/src/main-built.js" id="main-script"></script>#g' assets/app.xml
 	scp -ri $PRIVATEKEY_PATH * root@104.236.20.173:/var/www/test.hangoutsplay.com
+	ssh -i $PRIVATEKEY_PATH root@104.236.20.173 'forever restart /var/www/test.hangoutsplay.com/app.js --prod'
 fi
