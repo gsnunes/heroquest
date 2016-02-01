a6d7a13a524e402498192e3fe46aaa126caf9ff3
Guilherme Nunes <guinunesilva@gmail.com>
2016-02-01 15:00:27 -0200

    jukebox state

--- assets/js/src/view/JukeboxTabView.js
+++ assets/js/src/view/JukeboxTabView.js
@@ -12,8 +12,18 @@ define([
 
 
 		events: {
-			'click .stop': 'stop',
-			'click .play-now': 'playTrack',
+			'click .stop': function (ev) {
+				var id = $(ev.target).parents('li').attr('id');
+				this.stopTrack(id);
+
+				gapi.hangout.data.setValue('jukebox', JSON.stringify(id));
+			},
+			'click .play-now': function (ev) {
+				var oEmbed = $(ev.target).parents('li').data('oEmbed');
+				this.playTrack(oEmbed);
+
+				gapi.hangout.data.setValue('jukebox', JSON.stringify(oEmbed));
+			},
 			'click .add-track': 'embedTrack',
 			'click .remove-track': 'removeTrack'
 		},
@@ -22,9 +32,53 @@ define([
 		initialize: function () {
 			this.load = false;
 
+			this.bindEvents();
+
 			SC.initialize({
 				client_id: 'c846a9e67cb901619c96c9d06fb0f2e2'
 			});
+		},
+
+
+		afterRender: function () {
+			this.populatePlaying();
+		},
+
+
+		/**
+		 * populatePlaying
+		 */
+		populatePlaying: function () {
+			var data = JSON.parse(gapi.hangout.data.getValue('jukebox')) || [];
+		},
+
+
+		bindEvents: function () {
+			var i,
+				len,
+				currentMeta,
+				_this = this;
+
+			gapi.hangout.data.onStateChanged.add(_.bind(function (ev) {
+				len = ev.addedKeys.length;
+
+				if (len) {
+					for (i = 0; i < len; i++) {
+						currentMeta = ev.addedKeys[i];
+
+						if (currentMeta.key === 'jukebox' && currentMeta.lastWriter !== gapi.hangout.getLocalParticipant().id) {
+							var value = JSON.parse(currentMeta.value);
+
+							if (typeof value === 'string') {
+								_this.stopTrack(value);
+							}
+							else {
+								_this.playTrack(value);
+							}
+						}
+					}
+				}
+			}, this));
 		},
 
 
@@ -49,14 +103,16 @@ define([
 		},
 
 
-		stop: function (ev) {
-			$(ev.target).parents('li').remove();
+		stopTrack: function (id) {
+			this.$('.playing').find('#' + id).remove();
 		},
 
 
-		playTrack: function (ev) {
-			var oEmbed = $(ev.target).parents('li').data('oEmbed');
-			this.$('.playing').append('<li>' + oEmbed.html.replace('visual=true&', '').replace('height=\"400\"', 'height=\"80\"') + '<button type="button" class="btn btn-danger btn-xs stop">stop</button></li>')
+		playTrack: function (oEmbed) {
+			var item = $('<li id="' + oEmbed.randomId + '">' + oEmbed.html.replace('visual=true&', '').replace('height=\"400\"', 'height=\"80\"') + '<button type="button" class="btn btn-danger btn-xs stop">stop</button></li>');
+
+			this.$('.playing').append(item);
+			item.data('oEmbed', oEmbed);
 		},
 
 
@@ -108,6 +164,8 @@ define([
 				show_comments: false,
 				default_width: 415
 			}).then(function (oEmbed) {
+				oEmbed.randomId = (new Date()).getTime();
+
 				_this.addTrack(oEmbed);
 				_this.updateLocalStorage();
 				$(ev.target).parents('.input-group').find('.track-url').val('');
